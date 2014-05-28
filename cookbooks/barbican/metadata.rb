55c2305c76c277c025a59d4b89a3d43295edbaa0
Igor Gueths <igor.gueths@rackspace.com>
2014-05-28 16:26:30 -0500

    Explicitly version Yum dependency to >= 3.0.0.

--- cookbooks/barbican/metadata.rb
+++ cookbooks/barbican/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures barbican'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.8'
 
-depends          'yum'
+depends          'yum', '>= 3.0.0'
 depends          'yum-epel'
 depends          'authorized_keys'
 depends          'ntp'
